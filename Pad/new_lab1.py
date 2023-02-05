5881a345a5d8746a78529873d27010b5576bbdec
Timur Gayazov <tim.gayazov@gmail.com>
2023-02-06 00:00:07 +0400

    fixed bugs and add comments to the code Pad

--- Pad/new_lab1.py
+++ Pad/new_lab1.py
@@ -8,7 +8,7 @@ while True:
     check = int(input("Введите 0 или 1 для ручного или автоматического заполнения списка: "))
 
 
-    def manual_filling():
+    def manual_filling(): # функция ручной генерации списка с сиспользованием стандартных функций (append)
         count_elem = int(input("Введите количество элементов списка: "))
 
         for i in range(count_elem):
@@ -17,7 +17,7 @@ while True:
         mas.append(999)
 
 
-    def manual_filling_wo_func(mas):
+    def manual_filling_wo_func(mas): # функция ручной генерации списка без использования стандартных функций
         count_elem = int(input("Введите количество элементов списка: "))
         for i in range(0, count_elem):
             mas+=[int(input())]
@@ -25,11 +25,11 @@ while True:
         mas+=[int(999)]
 
     min_row_id = 0
-    def main_func(mas):
+    def main_func(mas): # главная функция в которой происходит обработка списка
         min_row = 999
         min_row_id = 0
         k = 0
-        for i in range(0, len(mas)):
+        for i in range(0, len(mas)): # поиск цепочек четных чисел, поиск минимального элемента в этих цепочках
             if (mas[i] %2 == 0):
                 if (mas[i] < min_row and mas[i] != min_row):
                     min_row = mas[i]
@@ -50,7 +50,7 @@ while True:
                 k = 0
         del mas[-1]
         ii = 0
-        while " " in mas:
+        while " " in mas: # удаление элементов списка, являющиеся минимум в своих цепочках
             if mas[ii] == " ":
                 del mas[ii]
             ii+=1
@@ -59,7 +59,7 @@ while True:
         print(" ")
 
 
-    def with_func(mas):
+    def with_func(mas): # определение метода генерации списка
         if check == 0:
             if check_method == 0:
                 manual_filling()
@@ -83,7 +83,7 @@ while True:
             print("Ошибка ввода")
             sys.exit()
 
-
+    # определение метода (с использованием стандартных функций или без)
     if check_method == 0: with_func(mas)
     elif check_method == 1: with_func(mas)
     else: print("Введите только 0 или 1")
