a2e3d5efa7cefea8639266450ad36bee44513734
Timur Gayazov <tim.gayazov@gmail.com>
2023-02-09 22:41:21 +0400

    added repoets and fixed bugs

--- Pad/lab2/lab2.py
+++ Pad/lab2/lab2.py
@@ -1,10 +1,11 @@
-#UTF-8
+# UTF-8
 import numpy as np
 import sys
 import os
 import random
 
-def hidden_func(mat_copy, mat): # функция которая записывает исходные и конечные данные в txt файл
+
+def hidden_func(mat_copy, mat):  # функция которая записывает исходные и конечные данные в txt файл
     np.savetxt("lab2_1.txt", mat_copy, fmt='%.1d')
     np.savetxt("lab2_2.txt", mat, fmt='%.1d')
     open("result_lab2.txt", "w").write(open("lab2_1.txt", "r").read() + "\n" + open("lab2_2.txt", "r").read())
@@ -12,44 +13,40 @@ def hidden_func(mat_copy, mat): # функция которая записыва
     os.remove("lab2_2.txt")
 
 
-
-def main_func(mat, m, n): # функция поиска среднего значения каждого столбца и строчки
+def main_func(mat, m, n):  # функция поиска среднего значения каждого столбца и строчки
     print(mat)
     print(" ")
     mat_copy = mat
     list = []
     list1 = []
 
-    # print("-----")
     for i in range(0, m):
-        # print(*mat[i], "---", sum(mat[i]) // m)
         list1.append([sum(mat[i]) // m])
-    # print("-----")
 
     new_row = np.array(list)
     for i in range(0, n):
-        # print(*mat[:, i], "---", sum(mat[:, i]) // n)
         list.append(sum(mat[:, i]) // n)
     list1.append([sum(list) // len(list)])
     new_row = np.array(list)
     mat = np.vstack([mat, new_row])
     mat = np.c_[mat, list1[:]]
-    # print(new_row, "строка")
-    print("Результат матрица вида: ", m+1,"X",n+1)
+
+    print("The result is a matrix of the form: ", m + 1, "X", n + 1)
     print(mat)
+    print("")
+    print("The original matrix and the matrix with the result were written to a file: 'result_lab2.txt'")
+    print("")
     hidden_func(mat_copy, mat)
 
 
-
-
-while True: # рандоманая генерация матрицы
-    check = int(input("Введите 1 для автоматического заполнения матрицы: "))
+while True:  # рандоманая генерация матрицы
+    check = int(input("Enter 1 for random filling matrix: "))
     if check == 1:
         m = random.randint(2, 10)  # столбец
         n = random.randint(2, 10)  # строка
-        mat = np.random.randint(0, 20, (m, n))  # столбец, строка
-        print("Матрица вида:", m, "X", n)
+        mat = np.random.randint(0, 20, (m, n))
+        print("Matrix of the form:", m, "X", n)
         main_func(mat, m, n)
     else:
-        print("Ощибка ввода")
+        print("Input error, try again!")
         sys.exit()
--- Pad/lab3/lab3.py
+++ Pad/lab3/lab3.py
@@ -2,60 +2,74 @@ from pathlib import Path
 import csv
 
 
-# folder_name = input("folder name: ")
-# folder = Path(folder_name)
-# if folder.is_dir():
-#     folder_count = len([1 for file in folder.iterdir()])
-#
-# print(f"В папке {folder_name} есть {folder_count} объектов")
+def count_obj():  # функция подсчета объектов в определенной директории
+    folder_name = input("Enter folder name: ")
+    folder = Path(folder_name)
+    if folder.is_dir():
+        folder_count = len([1 for file in folder.iterdir()])
+
+    print(f"In folder {folder_name} there {folder_count} objects.")
 
 
-def main():
-    def write_csv():  # функция записи новыой строки в файл
-        key_inp = input('Enter key: ')
-        date_inp = input('Enter date: ')
-        price_inp = input('Enter price: ')
-        name_inp = input('Enter name: ')
-        all_data[key_inp] = [date_inp, price_inp, name_inp]
-        with open('data.csv', 'w') as f:
-            for elem in all_data:
-                f.write(elem + "," + all_data[elem][0] + "," + all_data[elem][1] + "," + all_data[elem][2] + "\n")
-        f.close()
+def write_csv(all_data):  # функция записи новыой строки в файл
+    key_inp = input('Enter key: ')
+    date_inp = input('Enter date: ')
+    price_inp = input('Enter price: ')
+    name_inp = input('Enter name: ')
+    all_data[key_inp] = [date_inp, price_inp, name_inp]
+    with open('data.csv', 'w') as f:
+        for elem in all_data:
+            f.write(elem + "," + all_data[elem][0] + "," + all_data[elem][1] + "," + all_data[elem][2] + "\n")
+    f.close()
 
-    all_data = {}
-    # чтение данных из файла и их занесение в словарь
+
+def write_to_dict(all_data):  # функция чтения данных из файла и их занесения их в словарь
     with open("data.csv", "r") as f:
         # reader = csv.DictReader(f)
         reader = csv.reader(f)
         for line in reader:
             all_data[line[0]] = line[1:]
 
-    print("ID, Date, Price, Product Name")
-    for elem in all_data:
-        print(elem, *all_data[elem])
-    print("\n")
 
-    # Сортировка по числовому полю
+def sort_num_field(all_data):  # функция Сортировки словаря по числовому полю
     print("Сортировка словаря по цене:")
     for elem in sorted(all_data.items(), key=lambda para: int(para[1][1])):
         print(elem[0], *elem[1])
     print("")
-    # Сортировка по строковому полю
+
+
+def sort_str_field(all_data):  # функция сортировки словаря по строковому полю
     print("Сортировка словаря по названию товара:")
     for elem in sorted(all_data.items(), key=lambda para: para[1][2]):
         print(elem[0], *elem[1])
     print("")
 
-    # Вывод строк словаря в которых наименование товара = 'pizza'
+
+def inference_by_criterion(all_data):  # функция вывода строк словаря в которых наименование товара = 'pizza'
     print("Строки, в которых имя товара 'pizza': ")
     for elem in all_data:
         if all_data[elem][2] == 'pizza':
             print(elem, *all_data[elem])
 
+
+def main():
+    all_data = {}
+    count_obj()
+    write_to_dict(all_data)
+
+    print("ID, Date, Price, Product Name")
+    for elem in all_data:
+        print(elem, *all_data[elem])
+    print("\n")
+
+    sort_num_field(all_data)
+    sort_str_field(all_data)
+    inference_by_criterion(all_data)
     print("")
+
     check = input('Add newline to file? Enter y/n: ')
     if check == 'y':
-        write_csv()
+        write_csv(all_data)
         print("Данные успешно сохранены!")
     else:
         print("Программа остановлена")
