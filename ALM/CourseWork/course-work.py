4612aacea460e2707adb3177e76456ee3ca4f568
Timur Gayazov <tim.gayazov@gmail.com>
2024-03-14 20:15:57 +0400

    the code of the course work has been completed

--- ALM/CourseWork/course-work.py
+++ ALM/CourseWork/course-work.py
@@ -1,11 +1,10 @@
-from matrix import *
+import time
+from matrix import Y_matrix, W_matrix, U_matrix
 
 YArr = ['', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+states = {'S0': [0, 0, 0, 0], 'S1': [0, 0, 0, 1], 'S2': [0, 0, 1, 0], 'S3': [0, 0, 1, 1], 'S4': [0, 1, 0, 0],
+          'S5': [0, 1, 0, 1], 'S6': [0, 1, 1, 0], 'S7': [0, 1, 1, 1], 'S8': [1, 0, 0, 0], 'S9': [1, 0, 0, 1]}
 s_line = '-' * 30
-
-
-def matrix():
-    pass
 
 
 def check_state(XArr, state, count):
@@ -14,7 +13,93 @@ def check_state(XArr, state, count):
         graph_scheme(XArr, state, count)
     if count == 0:
         print(f'{s_line}\nLooping...')
+        print(f'{(time.perf_counter() - start_time) / 1e6} sec.')
         exit()
+
+
+def update_matrix_state(XArr, current_state, next_state, state_history):
+    if state_history.count(current_state) > 1:
+        print(f'Looping...\n{s_line}')
+        print(f'{(time.perf_counter() - start_time) / 1e6} sec.')
+        exit()
+    elif current_state == next_state:
+        print(f'Looping...\n{s_line}')
+        print(f'{(time.perf_counter() - start_time) / 1e6} sec.')
+        exit()
+
+    current_state = next_state
+
+    if current_state == 'S0':
+        print(s_line)
+        print(f'{(time.perf_counter() - start_time) / 1e6} sec.')
+        exit()
+    matrix(XArr, current_state, state_history)
+
+
+def matrix(XArr, current_state, state_history):
+    next_state = ''
+    y_count = 0
+    w_count = 0
+    u_count = 0
+    state_code = states[current_state].copy()
+    for j in range(1, len(YArr)):
+        YArr[j] = 0
+
+    for yi in Y_matrix:
+        y_count += 1
+        for y in yi:
+            if (y[0] == states[current_state][0] and y[1] == states[current_state][1] and
+                    y[2] == states[current_state][2] and y[3] == states[current_state][3]):
+                active_x_count = 0
+                success_x_count = 0
+                for elementy in range(4, len(y)):
+                    if y[elementy] != 2:
+                        active_x_count += 1
+                    if XArr[elementy - 3] == y[elementy]:
+                        success_x_count += 1
+                if active_x_count == success_x_count:
+                    # print(y, y_count, active_x_count, success_x_count)
+                    YArr[y_count] = 1
+
+    for wi in W_matrix:
+        w_count += 1
+        for w in wi:
+            if (w[0] == states[current_state][0] and w[1] == states[current_state][1] and
+                    w[2] == states[current_state][2] and w[3] == states[current_state][3]):
+                active_x_count = 0
+                success_x_count = 0
+                for elementy in range(4, len(w)):
+                    if w[elementy] != 2:
+                        active_x_count += 1
+                    if XArr[elementy - 3] == w[elementy]:
+                        success_x_count += 1
+                if active_x_count == success_x_count:
+                    # print(w, f'w{w_count}', active_x_count, success_x_count)
+                    state_code[w_count - 1] = 1
+                    # print(state_code)
+    for ui in U_matrix:
+        u_count += 1
+        for u in ui:
+            if (u[0] == states[current_state][0] and u[1] == states[current_state][1] and
+                    u[2] == states[current_state][2] and u[3] == states[current_state][3]):
+                active_x_count = 0
+                success_x_count = 0
+                for elementy in range(4, len(u)):
+                    if u[elementy] != 2:
+                        active_x_count += 1
+                    if XArr[elementy - 3] == u[elementy]:
+                        success_x_count += 1
+                if active_x_count == success_x_count:
+                    # print(u, f'u{u_count}', active_x_count, success_x_count)
+                    state_code[u_count - 1] = 0
+                    # print(state_code)
+    for key, value in states.items():
+        if value == state_code:
+            next_state = key
+
+    print(f'{current_state} -> {" ".join(map(str, YArr))} -> {next_state}')
+    state_history.append(current_state)
+    update_matrix_state(XArr, current_state, next_state, state_history)
 
 
 def graph_scheme(XArr, state, count):
@@ -82,6 +167,7 @@ def graph_scheme(XArr, state, count):
         # S1 -> S1
         if XArr[9] == '0':
             print(f'S1 -> {" ".join(map(str, YArr))} -> S1\nLooping...\n{s_line}')
+            print(f'{(time.perf_counter() - start_time) / 1e6} sec.')
             exit()
         # S1 -> S2
         elif XArr[9] == '1':
@@ -152,6 +238,7 @@ def graph_scheme(XArr, state, count):
         # S3 -> S3
         if XArr[14] == '0':
             print(f'S3 -> {" ".join(map(str, YArr))} -> S3\nLooping...\n{s_line}')
+            print(f'{(time.perf_counter() - start_time) / 1e6} sec.')
             exit()
         # S3 -> S6
         elif XArr[14] == '1':
@@ -174,6 +261,7 @@ def graph_scheme(XArr, state, count):
         # S4 -> S4
         elif XArr[15] == '0':
             print(f'S4 -> {" ".join(map(str, YArr))} -> S4\nLooping...\n{s_line}')
+            print(f'{(time.perf_counter() - start_time) / 1e6} sec.')
             exit()
         # S4 -> S5
         elif XArr[15] == '1' and XArr[11] == '1' and XArr[13] == '1':
@@ -223,6 +311,7 @@ def graph_scheme(XArr, state, count):
         # S5 -> S5
         elif XArr[8] == '0':
             print(f'S5 -> {" ".join(map(str, YArr))} -> S5\nLooping...\n{s_line}')
+            print(f'{(time.perf_counter() - start_time) / 1e6} sec.')
             exit()
         # S5 -> S6
         elif XArr[8] == '1' and XArr[7] == '0':
@@ -252,6 +341,7 @@ def graph_scheme(XArr, state, count):
         # S7 -> S7
         elif XArr[9] == '0':
             print(f'S7 -> {" ".join(map(str, YArr))} -> S7\nLooping...\n{s_line}')
+            print(f'{(time.perf_counter() - start_time) / 1e6} sec.')
             exit()
     # --S8--
     # S8 -> S9
@@ -272,10 +362,12 @@ def graph_scheme(XArr, state, count):
             YArr[25] = 1
             print(f'S9 -> {" ".join(map(str, YArr))} -> S0\n{s_line}')
             YArr[25] = 0
+            print(f'{(time.perf_counter() - start_time) / 1e6} sec.')
             exit()
         # S9 -> S9
         elif XArr[15] == '0':
             print(f'S9 -> {" ".join(map(str, YArr))} -> S9\nLooping...\n{s_line}')
+            print(f'{(time.perf_counter() - start_time) / 1e6} sec.')
             exit()
 
 
@@ -283,24 +375,28 @@ while True:
     with open('input-course.txt', 'r') as file:
         line = file.readline().strip()
 
-    if len(line) == 15:
-        XArr = list(line)
-    else:
-        print("Ошибка: Длина строки не равна 15")
+    XArr = list(line) if len(line) == 15 else print("Ошибка: Длина строки не равна 15") & exit()
 
     print('_' * 31)
     print(f'|          X1 - X15           |'
           f'\n|{" ".join(map(str, XArr))}|')
     print('|' + '_' * 29 + '|')
-    XArr.insert(0, '#')
+    XArr.insert(0, '2')
 
     method = input('Введите m для матрицы или g для граф-схемы: ')
 
     if method == 'm':
-        matrix()
+        state_history = []
+        print(f'{s_line}\nStart S -> Y1-Y25 -> End S')
+        current_state = 'S0'
+        for xmas in range(0, len(XArr)):
+            XArr[xmas] = int(XArr[xmas])
+        start_time = time.perf_counter()
+        matrix(XArr, current_state, state_history)
     elif method == 'g':
         state = 'S0'
         print(f'{s_line}\nStart S -> Y1-Y25 -> End S')
+        start_time = time.perf_counter()
         graph_scheme(XArr, state, 1)
     elif method == 'e':
         print('Выход из программы.')
